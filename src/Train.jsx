a3eadeecd6fa888f9a45c6e9170b49911f6e242e
tomas-b <tomas.b.lp@gmail.com>
2021-08-03 03:02:02 -0300

    train && download

--- src/Train.jsx
+++ src/Train.jsx
@@ -2,71 +2,80 @@ import React, { useState, useEffect, useRef } from "react";
 import Webcam from "react-webcam";
 import * as tf from "@tensorflow/tfjs";
 
+let model = tf.sequential();
+
 const Train = () => {
 
-  let [data, setData] = useState(null)
+  let [data, setData] = useState(null);
+  let [loss, setLoss] = useState(null);
 
   const train = () => {
-
     let json = JSON.parse(data);
     let keys = Object.keys(json);
 
-    let _xs = []
-    let _ys = []
+    let _xs = [];
+    let _ys = [];
 
-    for(const [i, key]  of keys.entries()) {
-        // let y = keys.map( k => k === key ? 1 : 0 )
-        let y = i
-        json[key].map( pose => {
-          let x = pose.map( ({x,y}) => ([x,y]) ).flat() 
-          _xs.push(x)
-          _ys.push(y)
-        })
+    for (const [i, key] of keys.entries()) {
+      // let y = keys.map( k => k === key ? 1 : 0 )
+      let y = i;
+      json[key].map((pose) => {
+        let x = pose.map(({ x, y }) => [x, y]).flat();
+        _xs.push(x);
+        _ys.push(y);
+      });
     }
 
-    console.log(_xs, _ys)
+    console.log(_xs, _ys);
 
-    let labelTensor = tf.tensor1d( _ys, 'int32' )
-    let xs = tf.tensor2d(_xs)
-    let ys = tf.oneHot(labelTensor, keys.length)
+    let labelTensor = tf.tensor1d(_ys, "int32");
+    let xs = tf.tensor2d(_xs);
+    let ys = tf.oneHot(labelTensor, keys.length);
 
     console.log(xs.shape, ys.shape);
-    xs.print()
-    ys.print()
-
-
-    let model = tf.sequential()
+    xs.print();
+    ys.print();
 
     let hidden = tf.layers.dense({
       units: 16,
-      activation: 'sigmoid',
-      inputDim: 34
-    })
+      activation: "sigmoid",
+      inputDim: 34,
+    });
 
     let output = tf.layers.dense({
       units: keys.length,
-      activation: 'softmax'
-    })
+      activation: "softmax",
+    });
 
-    model.add(hidden)
-    model.add(output)
+    model.add(hidden);
+    model.add(output);
 
-    let optimizer = tf.train.sgd(.2)
+    let optimizer = tf.train.sgd(0.2);
 
     model.compile({
       optimizer,
-      loss: 'categoricalCrossentropy'
-    })
+      loss: "categoricalCrossentropy",
+    });
 
-    model.fit(xs, ys).then(res => console.log(res))
+    model
+      .fit(xs, ys, {
+        epochs: 150,
+        shuffle: true,
+      })
+      .then(async (res) => {
+        console.log(res);
+        const saveResult = await model.save("downloads://model");
+        console.log(saveResult);
+      });
+  };
 
-  }
-
-  return <>
-  <button onClick={train}>run</button>
-  <textarea onChange={e=>setData(e.target.value)}></textarea>
-  </>
-
+  return (
+    <>
+      <button onClick={train}>run</button>
+      <textarea onChange={(e) => setData(e.target.value)}></textarea>
+      <p>loss: {loss}</p>
+    </>
+  );
 };
 
 export default Train;
